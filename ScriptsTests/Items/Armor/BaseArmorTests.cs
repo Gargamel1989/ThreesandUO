026324c10a06ef7c896301e4eaca73888076d875
Gargamel <joeper_100@hotmail.com>
2015-10-19 18:48:45 +0200

    added tests

---
+++ ScriptsTests/Items/Armor/BaseArmorTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server;
+using Server.Items;
+using Server.Misc;
+using Server.Network;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Server.Items
+{
+    [TestClass()]
+    public class BaseArmorTests
+    {
+    }
+}
