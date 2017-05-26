572880fcbbbed8b1f4ad4c714feea27cf960b7af
Drabzes <reynders_giel@hotmail.com>
2017-05-26 16:37:09 +0200

    Added disturb functions to the class hide.cs

--- Scripts/Skills/Utility/Hiding/Hide.cs
+++ Scripts/Skills/Utility/Hiding/Hide.cs
@@ -4,23 +4,43 @@ using Server;
 
 namespace Scripts.Skills.Utility.Hiding
 {
+    public enum HidingState
+    {
+        None = 0,
+        TryingToHide = 1,    // We are in the process of hiding (that is, waiting GetHideTime()). Hiding may be interupted in this state.
+    }
     public abstract class Hide : IHiding
     {
-        
+        HidingState m_state;
+
         public bool IsHiding
         {
             get
             {
-                throw new NotImplementedException();
+                return m_state == HidingState.TryingToHide;
             }
         }
 
         public void OnHiderHurt()
         {
+            if (IsHiding)
+            {
+
+            }
             throw new NotImplementedException();
         }
 
         public void OnHiderKilled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disturb(DisturbType type)
+        {
+            Disturb(type, false);
+        }
+
+        private void Disturb(DisturbType type, bool v2)
         {
             throw new NotImplementedException();
         }
