7b76622b54d5c58ea1a1db5a9155014d5e56040b
Drabzes <reynders_giel@hotmail.com>
2017-05-26 16:24:26 +0200

    Created Hide
    Emprty abstract class hide based on IHiding

---
+++ Scripts/Skills/Utility/Hiding/Hide.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Server;
+
+
+namespace Scripts.Skills.Utility.Hiding
+{
+    public abstract class Hide : IHiding
+    {
+        
+        public bool IsHiding
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void OnHiderHurt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnHiderKilled()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
